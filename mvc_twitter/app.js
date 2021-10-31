424f374211d2881e8821d9053aedb47adfd01508
Min Jae Kim <exploit017@gmail.com>
2021-10-31 16:51:22 +0900

    코드 구조 리팩토링으로 인해 app.js 수정

--- mvc_twitter/app.js
+++ mvc_twitter/app.js
@@ -5,13 +5,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const index = require('./server/routes/index')
+const users = require('./server/routes/users')
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -26,8 +26,8 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
