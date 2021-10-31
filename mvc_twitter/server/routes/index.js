07e63fea741bd5a9e9dca324d3f754e43b91f198
Min Jae Kim <exploit017@gmail.com>
2021-10-31 17:39:48 +0900

    login, signup, profile 페이지 및 라우터 완성

--- mvc_twitter/server/routes/index.js
+++ mvc_twitter/server/routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/login', getLogin(req, res, next));
+router.get('/signup', getSignUp(req, res));
+router.get('/profile', getProfile(req, res, next));
+
+const getLogin = (req, res, next) => {
+  res.render('login', { title: 'Login Page', message: req.flash('loginMessage') });
+};
+
+const getSignUp = (req, res) => {
+  res.render('signup', { title: 'Signup Page', message: req.flash('signupMessage')});
+};
+
+const getProfile = (req, res, next) => {
+  res.render('profile', { 
+    title: 'Profile Page', 
+    user: req.user, 
+    avatar: gravatar.url(req.user.email, { s: '100', r: 'x', d: 'retro' }, true),
+  });
+};
 
 module.exports = router;
